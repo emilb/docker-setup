682e7d4a1af2308c30971daac63d710f794581c0
Emil Breding <emil.breding@viaplay.com>
2015-11-19 22:24:39 +0100

    added x flag

--- docker-services/pull-services.sh
+++ docker-services/pull-services.sh
@@ -1,9 +1,10 @@
 docker pull graylog2/allinone
 docker pull crosbymichael/skydock
 docker pull crosbymichael/skydns
-docker pull linuxserver/nzbget
-docker pull kibana
+docker pull jwilder/nginx-proxy
 docker pull tutum/influxdb
 docker pull grafana/grafana
+
+docker pull linuxserver/nzbget
 docker pull mysql
-docker pull jwilder/nginx-proxy
+
--- setup.sh
+++ setup.sh
@@ -20,11 +20,18 @@ source passwords.sh
 echo "Base install..."
 base/base-install.sh
 
-echo "Locale fix..."
+echo "Post install..."
 base/post-install.sh
 
 echo "Installing Docker..."
 docker-services/docker.sh
 
+echo "Pulling docker containers"
+docker-services/pull-services.sh
+
+echo "Adding systemd for docker containers"
+docker-services/add-systemd-startup.sh
+
 echo "Adding users..."
 base/users-install.sh
+
