d2830ab821284b3fdc144cfcd0e8e81fd0d6e50b
Emil Breding <emil.breding@viaplay.com>
2015-11-20 21:43:48 +0100

    removed default user

--- config.sh
+++ config.sh
@@ -2,6 +2,6 @@
 export domain="northpath.se"
 export docker_base_path="/data"
 export password_keys="emil patrik admin root_user mysql_root mysql_newznab grafana influx graylog graylog_server_secret"
-export users="emil patrik admin"
+export users="patrik admin"
 export allowed_samba_nets="213.185.253.3 "
 export subdomains="www nzbs nzbget tv plex graylog grafana vpn"
